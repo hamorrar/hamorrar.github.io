a687f34a3f364a67c7c8bed56a3739dd5ba78dd8
Hilal Morrar <hilalmorrar@gmail.com>
2025-05-28 15:20:46 -0500

    added name email and pic

--- app/page.tsx
+++ app/page.tsx
@@ -206,10 +206,10 @@ export default function Portfolio() {
               className="mb-8"
             >
               <Image
-                src="/placeholder.svg?height=120&width=120"
+                src="/me.png?height=120&width=120"
                 alt="Hilal Morrar"
-                width={120}
-                height={120}
+                width={220}
+                height={220}
                 className="rounded-full border-4 border-border"
                 priority
               />
@@ -229,9 +229,11 @@ export default function Portfolio() {
               </p>
 
               <div className="flex justify-center mt-8">
+                <Link href="mailto:hilalmorrar@gmail.com" rel="noopener noreferrer" target="_blank">
                 <Button size="lg" className="rounded-full">
                   Get in Touch
                 </Button>
+                </Link>
               </div>
             </motion.div>
           </div>
@@ -423,14 +425,19 @@ export default function Portfolio() {
               © {new Date().getFullYear()} Hilal Morrar. All rights reserved.
             </p>
             <div className="flex items-center space-x-4 mt-4 md:mt-0">
+              <Link href="https://github.com/hamorrar" rel="noopener noreferrer" target="_blank">
               <Button variant="ghost" size="sm">
-                <Github className="h-4 w-4 mr-2" />
+                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
                 GitHub
               </Button>
+              </Link>
+
+              <Link href="mailto:hilalmorrar@gmail.com" rel="noopener noreferrer" target="_blank">
               <Button variant="ghost" size="sm">
                 <Mail className="h-4 w-4 mr-2" />
                 Email
               </Button>
+              </Link>
             </div>
           </div>
         </div>
